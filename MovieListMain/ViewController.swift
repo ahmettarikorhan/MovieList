128e78e3a2d0abb0410cb495dd735a97dbd5bd44
OP <mac@macs-MacBook-Pro.local>
2024-08-09 15:29:13 +0300

    first commit

---
+++ MovieListMain/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieListMain
+//
+//  Created by mac on 8.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
